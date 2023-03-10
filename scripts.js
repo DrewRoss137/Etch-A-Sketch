43e3cd2eab9749a542aaad8567f1cdf37b01ee9b
DrewRoss137 <DrewRoss137@Gmail.Com>
2023-03-10 18:09:33 +0000

    Improve code readability

--- scripts.js
+++ scripts.js
@@ -1,27 +1,50 @@
 /* Elements */
-
 const body = document.getElementById("body");
 
-const etchASketchContainer = document.getElementById("etch-a-sketch-container");
+const container = document.getElementById("container");
 
-const colourWheel = document.getElementById("colour-wheel");
+const gridSizeInput = document.getElementById("grid-size-input");
+
+const colourWheel = document.getElementById("colour-input");
 
 const colourButton = document.getElementById("colour-button");
 
 const rainbowButton = document.getElementById("rainbow-button");
 
+const gridButton = document.getElementById("grid-button");
+
 const rubberButton = document.getElementById("rubber-button");
 
 const clearButton = document.getElementById("clear-button");
+/* Elements */
 
-for (let rows = 0; rows < 16; rows++) {
-  for (let columns = 0; columns < 16; columns++) {
-    const square = document.createElement("div");
-    square.id = "square";
-    etchASketchContainer.appendChild(square);
+/* Code */
+generateGrid(16);
+gridSizeInput.addEventListener("change", () => {
+    const gridSize = gridSizeInput.value;
+    removeGrid();
+    generateGrid(gridSize);
+  });
 
-    square.addEventListener("mouseover", () => {
-      square.style.backgroundColor = "black";
+// Generate grid function
+function generateGrid(size) {
+    for (let rows = 0; rows < size; rows++) {
+      for (let columns = 0; columns < size; columns++) {
+        const square = document.createElement("div");
+        square.id = "square";
+        container.appendChild(square);
+  
+        square.addEventListener("mouseover", () => {
+          square.style.backgroundColor = "black";
+        });
+      }
+    }
+  }
+  
+  function removeGrid() {
+    const squares = document.querySelectorAll("#square");
+    squares.forEach((square) => {
+      square.remove();
     });
   }
-}
+/* Code */
