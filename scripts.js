1269146042705648c203568a08afb46b9631ab5e
DrewRoss137 <DrewRoss137@Gmail.Com>
2023-03-11 19:59:36 +0000

    Create gridSizeInput dynamic placeholder text

--- scripts.js
+++ scripts.js
@@ -1,49 +1,52 @@
-/* Elements */
 const body = document.getElementById("body");
+
 const container = document.getElementById("container");
 const gridSizeInput = document.getElementById("grid-size-input");
-const colourInput = document.getElementById("colour-input");
-const colourButton = document.getElementById("colour-button");
-const rainbowButton = document.getElementById("rainbow-button");
-const gridButton = document.getElementById("grid-button");
-const rubberButton = document.getElementById("rubber-button");
-const clearButton = document.getElementById("clear-button");
-/* Elements */
-
-/* Code */
-generateGrid(16);
-
-gridSizeInput.addEventListener("input", () => {
-  const gridSize = parseInt(gridSizeInput.value);
-  if (gridSize <= 0 || gridSize > 100 || isNaN(gridSize)) {
-    alert("Invalid input. Please enter a number between 1 and 100.");
-    return;
-  }
-  removeGrid();
-  generateGrid(gridSize);
+gridSizeInput.addEventListener("blur", () => {
+  gridSizeInput.setAttribute("placeholder", "Grid Size");
 });
+gridSizeInput.addEventListener("focus", () => {
+  gridSizeInput.removeAttribute("placeholder");
+});
+gridSizeInput.addEventListener("input", () => {
+  const gridSizeInputValue = gridSizeInput.value;
+  removeGrid();
+  generateGrid(gridSizeInputValue);
+});
+
+const colourInput = document.getElementById("colour-input");
+
+const colourButton = document.getElementById("colour-button");
+
+const rainbowButton = document.getElementById("rainbow-button");
+
+const gridButton = document.getElementById("grid-button");
+
+const rubberButton = document.getElementById("rubber-button");
+
+const clearButton = document.getElementById("clear-button");
+
+generateGrid(16);
 
 function generateGrid(size) {
   container.style.gridTemplateColumns = `repeat(${size}, calc(960px / ${size}))`;
   container.style.gridTemplateRows = `repeat(${size}, calc(960px / ${size}))`;
-  for (let rows = 0; rows < size; rows++) {
-    for (let columns = 0; columns < size; columns++) {
+  for (let columns = 0; columns < size; columns++) {
+    for (let rows = 0; rows < size; rows++) {
       const square = document.createElement("div");
       square.id = "square";
       square.style.backgroundColor = "transparent";
-      container.appendChild(square);
-
       square.addEventListener("mouseover", () => {
         square.style.backgroundColor = "black";
       });
+      container.appendChild(square);
     }
   }
-}
+};
 
 function removeGrid() {
   const squares = document.querySelectorAll("#square");
   squares.forEach((square) => {
     square.remove();
   });
-}
-/* Code */
+};
