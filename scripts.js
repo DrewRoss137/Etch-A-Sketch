f8e2b78636cd2ee12a0280ba0ad5f62306a755f3
DrewRoss137 <DrewRoss137@Gmail.Com>
2023-03-13 18:24:14 +0000

    Create further functions, improve readability, and improve structure

--- scripts.js
+++ scripts.js
@@ -3,15 +3,12 @@ const body = document.getElementById("body");
 const container = document.getElementById("container");
 
 const gridSizeInput = document.getElementById("grid-size-input");
-
 gridSizeInput.addEventListener("blur", () => {
   gridSizeInput.setAttribute("placeholder", "Grid Size");
 });
-
 gridSizeInput.addEventListener("focus", () => {
   gridSizeInput.removeAttribute("placeholder");
 });
-
 gridSizeInput.addEventListener("input", (event) => {
   const gridSizeInputValue = gridSizeInput.value;
   if ((gridSizeInputValue <= 0 || gridSizeInputValue > 64 || isNaN(gridSizeInputValue)) && event.data !== null) {
@@ -37,17 +34,53 @@ invalidInputMessage.id = "invalid-input-message";
 invalidInputMessage.textContent = "Invalid Grid Size. Please Enter A Number Between 1 And 64."
 
 const colourInput = document.getElementById("colour-input");
+colourInput.addEventListener("blur", () => {
+  if (!colourMode) return;
+});
+colourInput.addEventListener("change", () => {
+  if (!colourMode) return;
+});
+colourInput.addEventListener("input", () => {
+  if (!colourMode) return;
+});
+
 
 const colourButton = document.getElementById("colour-button");
+colourButton.addEventListener("blur", () => {
+  colourMode = false;
+});
+colourButton.addEventListener("click", () => {
+  colourMode = true;
+});
 
 const rainbowButton = document.getElementById("rainbow-button");
+rainbowButton.addEventListener("blur", () => {
+  rainbowMode = false;
+});
+rainbowButton.addEventListener("focus", () => {
+  rainbowMode = true;
+});
 
 const gridButton = document.getElementById("grid-button");
+gridButton.addEventListener("click", () => {
+  squares.forEach((square) => {
+    if (square.style.borderStyle === "solid") {
+      square.style.borderStyle = "none";
+    } else {
+      square.style.borderStyle = "solid";
+    }
+  });
+});
 
 const rubberButton = document.getElementById("rubber-button");
+rubberButton.addEventListener("blur", () => {
+  rubberMode = false;
+});
+rubberButton.addEventListener("focus", () => {
+  rubberMode = true;
+});
 
 const clearButton = document.getElementById("clear-button");
-
 clearButton.addEventListener("click", () => {
   const squares = document.querySelectorAll("#square");
   squares.forEach((square) => {
@@ -60,6 +93,8 @@ const squares = [];
 let colourMode;
 let rainbowMode;
 let rubberMode;
+
+generateGrid(16);
 
 function generateGrid(size) {
   container.style.gridTemplateColumns = `repeat(${size}, calc(960px / ${size}))`;
@@ -75,89 +110,31 @@ function generateGrid(size) {
   };
 };
 
-generateGrid(16);
-
-function awd(size) {
-
-
-  colourInput.addEventListener("blur", () => {
-    if (!colourMode) return;
-  });
-
-  colourInput.addEventListener("change", () => {
-    if (!colourMode) return;
-  });
-
-  colourInput.addEventListener("input", () => {
-    if (!colourMode) return;
-  });
-
-  colourButton.addEventListener("blur", () => {
-    colourMode = false;
-  });
-
-  colourButton.addEventListener("click", () => {
-    colourMode = true;
-  });
-
-  rainbowButton.addEventListener("blur", () => {
-    rainbowMode = false;
-  });
-
-  rainbowButton.addEventListener("focus", () => {
-    rainbowMode = true;
-  });
-
-  rubberButton.addEventListener("blur", () => {
-    rubberMode = false;
-  });
-
-  rubberButton.addEventListener("focus", () => {
-    rubberMode = true;
-  });
-
-
-      const generateRGBA = () => {
-        const randomRed = Math.floor(Math.random() * 256);
-        const randomGreen = Math.floor(Math.random() * 256);
-        const randomBlue = Math.floor(Math.random() * 256);
-        const randomAlpha = Math.random();
-        return `rgba(${randomRed}, ${randomGreen}, ${randomBlue}, ${randomAlpha})`;
-      };
-
-      squares.forEach((square) => {
-        square.addEventListener("mouseover", () => {
-          if (rubberMode) {
-            square.style.backgroundColor = "";
-          } else if (colourMode) {
-            square.style.backgroundColor = colourInput.value;
-          } else if (rainbowMode) {
-            square.style.backgroundColor = generateRGBA();
-          }
-        });
-      })
-    
-
-  gridButton.addEventListener("click", () => {
-    squares.forEach((square) => {
-      if (square.style.borderStyle === "solid") {
-        square.style.borderStyle = "none";
-      } else {
-        square.style.borderStyle = "solid";
-      }
-    });
-  });
-
-  clearButton.addEventListener("click", () => {
-    squares.forEach((square) => {
-      square.style.backgroundColor = "";
-    });
-  });
-};
-
-
 function removeGrid() {
   squares.forEach((square) => {
     square.remove();
   });
+};
+
+function styleSquares() {
+  squares.forEach((square) => {
+    square.addEventListener("mouseover", () => {
+      if (rubberMode) {
+        square.style.backgroundColor = "";
+      } else if (colourMode) {
+        square.style.backgroundColor = colourInput.value;
+      } else if (rainbowMode) {
+        square.style.backgroundColor = generateRGBA();
+      }
+    });
+  })
+};
+styleSquares();
+
+function generateRGBA() {
+  const randomRed = Math.floor(Math.random() * 256);
+  const randomGreen = Math.floor(Math.random() * 256);
+  const randomBlue = Math.floor(Math.random() * 256);
+  const randomAlpha = Math.random();
+  return `rgba(${randomRed}, ${randomGreen}, ${randomBlue}, ${randomAlpha})`;
 };
